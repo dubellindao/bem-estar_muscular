ab3a2d75adef4632c532f0b42e4bf4c1c34ac1f4
luissoaresgit <henrique.luis@escolar.ifrn.edu.br>
2024-09-11 20:34:33 -0300

    Criando os models

--- dinamicos/admin.py
+++ dinamicos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Usuario, Ficha_treino, Calculo_imc
 # Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    fields =  ['nome']
+
+@admin.register(Ficha_treino)
+class Ficha_treinoAdmin(admin.ModelAdmin):
+    fields = ['data_criacao']
+
+@admin.register(Calculo_imc)
+class Calculo_imcAdmin(admin.ModelAdmin):
+    fields = ['imc']
--- dinamicos/models.py
+++ dinamicos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+        
+class Calculo_imc(models.Model):
+    data = models.DateField()
+    imc = models.CharField(max_length=50)
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.imc
+
+class Ficha_treino(models.Model):
+    data_criacao = models.DateField()
+    segunda_feira = models.CharField(max_length=100)
+    terca_feira = models.CharField(max_length=100)
+    quarta_feira = models.CharField(max_length=100)
+    quinta_feira = models.CharField(max_length=100)
+    sexta_feira = models.CharField(max_length=100)
+    sabado = models.CharField(max_length=100)
+    domingo = models.CharField(max_length=100)
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
+
+    def __str__ (self):
+        return self.data_criacao
