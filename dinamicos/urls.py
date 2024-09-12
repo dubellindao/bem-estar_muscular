2a7aeef2bbf948ab1389a6c1ba700409b396f975
dubellindao <emanuel.brito@escolar.ifrn.edu.br>
2024-09-11 23:39:41 -0300

    criando conteúdo inicial do site

--- dinamicos/urls.py
+++ dinamicos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, fichas_prontas, tela_imc, superiores, tela_login
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('fichas_prontas/', fichas_prontas, name='fichas_prontas'),
+    path('tela_imc/', tela_imc, name='tela_imc'),
+    path('superiores/', superiores, name='superiores'),
+    path('tela_login/', tela_login, name='tela_login'),
 ]
--- dinamicos/views.py
+++ dinamicos/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index (request):
     return render(request, 'dinamicos/index.html')
+
+def fichas_prontas (request):
+    return render(request, 'dinamicos/fichas_prontas.html')
+
+def tela_imc (request):
+    return render(request, 'dinamicos/tela_imc.html')
+
+def superiores (request):
+    return render(request, 'dinamicos/superiores.html')
+
+def tela_login (request):
+    return render(request, 'dinamicos/tela_login.html')
